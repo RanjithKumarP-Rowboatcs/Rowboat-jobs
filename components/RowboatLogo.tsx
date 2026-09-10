389e67836b76a866822770e43d48c6f951404638
Rowboat-Consulting-Services <rowboatcs@gmail.com>
2026-09-10 10:45:20 +0530

    Fix Rowboat logo globe rendering

--- components/RowboatLogo.tsx
+++ components/RowboatLogo.tsx
@@ -2,12 +2,22 @@ type RowboatLogoProps = { className?: string }
 
 export default function RowboatLogo({ className = '' }: RowboatLogoProps) {
   return (
-    <div className={'rowboatLogo ' + className}>
-      <img
-        src="/rowboat-logo.svg"
-        alt="ROWBOAT CONSULTING SERVICES"
-        className="rowboatLogoImage"
-      />
+    <div className={'rowboatLogo ' + className} aria-label="ROWBOAT CONSULTING SERVICES">
+      <div className="rowboatLogoMain">
+        <span>ROWBOAT</span>
+        <img
+          src="/rowboat-mark.svg"
+          alt=""
+          aria-hidden="true"
+          className="rowboatLogoMark"
+        />
+        <span>CONSULTING</span>
+      </div>
+      <div className="rowboatLogoService" aria-hidden="true">
+        <i />
+        <span>SERVICES</span>
+        <i />
+      </div>
     </div>
   )
 }
