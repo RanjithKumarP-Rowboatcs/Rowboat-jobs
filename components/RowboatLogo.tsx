ce90339061b5ae7d91d6558e1ce02d839de6ea36
Rowboat-Consulting-Services <rowboatcs@gmail.com>
2026-09-10 11:08:24 +0530

    Fix Rowboat logo integration and prevent header cropping

--- components/RowboatLogo.tsx
+++ components/RowboatLogo.tsx
@@ -2,22 +2,11 @@ type RowboatLogoProps = { className?: string }
 
 export default function RowboatLogo({ className = '' }: RowboatLogoProps) {
   return (
-    <div className={'rowboatLogo ' + className} aria-label="ROWBOAT CONSULTING SERVICES">
-      <div className="rowboatLogoMain">
-        <span>ROWBOAT</span>
-        <img
-          src="/rowboat-mark.svg"
-          alt=""
-          aria-hidden="true"
-          className="rowboatLogoMark"
-        />
-        <span>CONSULTING</span>
-      </div>
-      <div className="rowboatLogoService" aria-hidden="true">
-        <i />
-        <span>SERVICES</span>
-        <i />
-      </div>
-    </div>
+    <img
+      src="/rowboat-logo.svg"
+      alt="ROWBOAT CONSULTING SERVICES"
+      className={className}
+      aria-label="ROWBOAT Consulting Services"
+    />
   )
 }
